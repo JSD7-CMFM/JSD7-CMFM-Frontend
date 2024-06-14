95361e7c36d5a691cdb9bb61c9b59d944aee0c85
Namfah01 <kanokwanbing09@gmail.com>
2024-06-14 14:11:29 +0700

    update productList

---
+++ src/features/homepage/components/ProductListByNf.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaStar } from "react-icons/fa6";
+import ProductCard from "./ProductCard";
+
+const ProductListHomePage = () => {
+  return (
+    <>
+      <ProductCard />
+    </>
+  );
+};
+
+export default ProductListHomePage;

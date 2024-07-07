485e7b11f393cbc83204333d979561e646f0c281
redrumkumg <n.ronnapat@github.com>
2024-07-07 22:54:38 +0700

    re push

--- src/apis/orders.js
+++ src/apis/orders.js
@@ -4,3 +4,5 @@ export const getOrders = () => axiosInstance.get(`/orders/`);
 export const getOrderById = (orderId) => axiosInstance.get(`/orders/${orderId}`);
 export const updateOrder = (orderId) => axiosInstance.patch(`/orders/${orderId}`);
 export const createOrder = (data) => axiosInstance.post(`/orders/`, data);
+
+

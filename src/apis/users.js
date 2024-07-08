e4759d6ccb8cdb689895cdab5e08419903c419a5
Bongkan <bongkan.jj@gmail.com>
2024-07-08 10:49:18 +0700

    add fetch data in profile page, login, signup

---
+++ src/apis/users.js
@@ -0,0 +1,56 @@
+import axiosInstance from "../config/myAPIs.js";
+import { setToken, getToken } from "../utils/localStorage.js";
+
+const Login = async (user) => {
+  try {
+    const response = await axiosInstance.post("/users/login", user);
+    console.log("Login response:", response.data);
+    const { token } = response.data;
+    if (response.data && token) {
+      setToken(token);
+    }
+    return response;
+  } catch (error) {
+    console.error("Login error:", error);
+    throw error;
+  }
+};
+
+const Register = async (data) => {
+  console.log(data);
+  try {
+    const response = await axiosInstance.post("/users/register", data);
+    const { token } = response.data;
+    console.log("response: ", response);
+    console.log("data: ", response.data);
+    console.log("token: ", token);
+
+    if (response.data && token) {
+      setToken(token);
+      return response;
+    }
+  } catch (error) {
+    console.error("Registration error:", error);
+    throw error;
+  }
+};
+
+const getUser = async (id) => {
+  const config = {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  };
+  return await axiosInstance.get(`/users/${id}`, config);
+};
+
+const editUser = async (id) => {
+  const config = {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  };
+  return await axiosInstance.patch(`/users/${id}`, config);
+};
+
+const getAllusers = async () => await axiosInstance.get("/users");
+
+const deleteUser = async () => await axiosInstance.delete("/users");
+
+export default { Login, Register, getUser, getAllusers, deleteUser, editUser };
--- src/features/accountprofile/components/AccountDetails.jsx
+++ src/features/accountprofile/components/AccountDetails.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import appAPI from "../../../apis/users.js";
+import { getToken } from "../../../utils/localStorage.js";
 
 const AccountDetails = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = getToken();
+        const getId = localStorage.getItem("id");
+        const response = await appAPI.getUser(getId);
+        console.log("Response data:", response);
+        b;
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div id="acc-info" className="px-7">
       <div>
-        <h2>Name Surname</h2>
-        <p className="mb-7">---------Email--------</p>
+        <h2>Email</h2>
+        <p className="mb-7">{user.data.email}</p>
       </div>
       <div>
         <p>Want to update your password?</p>

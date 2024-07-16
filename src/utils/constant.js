50b7ab445898a40caa9f8d436b8a8be03af5bb2a
Bongkan <bongkan.jj@gmail.com>
2024-07-16 15:10:02 +0700

    Change constant to BE

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
 // export const BASE_URL =  "https://jsd7-cmfm-backend.onrender.com";
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://jsd7-cmfm-backend-7wts.onrender.com";
 
 //test

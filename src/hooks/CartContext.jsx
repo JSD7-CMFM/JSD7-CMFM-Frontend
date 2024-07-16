9b40f41de52ed44e4b8a7717403c58b4d42781ee
Bongkan <bongkan.jj@gmail.com>
2024-07-16 20:05:08 +0700

    cart completed

--- src/hooks/CartContext.jsx
+++ src/hooks/CartContext.jsx
@@ -1,12 +1,31 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { getOrderById } from "../apis/orders.js";
+import { getCartState } from "../utils/localStorage.js";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const fetchCart = async () => {
+    const orderId = getCartState();
+    if (orderId === "No_cart") {
+      setCart([]);
+      return;
+    }
+    try {
+      const response = await getOrderById(orderId);
+      const products = response.data.cart_products;
+      setCart(products);
+    } catch (error) {
+      toast.error("fetching data error", error);
+    }
+  };
 
+  useEffect(() => {
+    fetchCart();
+  }, []);
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider value={{ cart, setCart, fetchCart }}>
       {children}
     </CartContext.Provider>
   );
